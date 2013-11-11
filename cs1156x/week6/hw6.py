523e1e6ffea875b85bc6932b0fa7946a5de66f5b
jbkim <jbremnant@gmail.com>
2013-11-10 23:15:11 -0500

    9/10. boo. got stupid question wrong

--- cs1156x/week6/hw6.py
+++ cs1156x/week6/hw6.py
@@ -15,7 +15,7 @@ import hw2 as h2
 
 """
 Overfitting and Deterministic Noise
-Question 1
+===================================
 
 Deterministic noise depends on H, as some models approx f better than others.
 Assume H' in H and that f is fixed. If we use H' instead of H, how does 
@@ -24,9 +24,10 @@ deterministic noise behave?
   [a] in general, deterministic noise will decrease?
 """
 
+
 """
 Overfitting and Regularization With Weight Decay
-Question 2,3,4,5,6
+================================================
 
   wget http://work.caltech.edu/data/in.dta  # training
   wget http://work.caltech.edu/data/out.dta # test
@@ -124,6 +125,8 @@ def readfile(fname):
       d.append(tok)
   return(d)
 
+# wow, much easier way exists:
+# np.genfromtxt("http://work.caltech.edu/data/in.dta", dtype=float)
 def getdata(fname='/home/jbkim/git/edx/cs1156x/week6/in.dta'):
   d = readfile(fname)
   x = np.array([i[0:2] for i in d])
@@ -202,3 +205,130 @@ def q6():
   lams = [10**k for k in ks]
   Eouts = map(lambda(x): runlm(lam=x)['Eout'], lams)
   return(Eouts)
+
+
+
+"""
+Regularization With Polynomials
+===============================
+
+nonlinear transform phi: x -> z, where phi transforms scalar x into vector z of
+Legendre polynomials (orthogonal),  z = (1,L1(x),L2(x),...,Lq(x))
+
+  H_Q = { h|h(x) = w'z = sum_{q=0}^{Q}(wq Lq(x)) }
+
+  L0(x) = 1
+
+
+7. Consider the following hypothesis set defined by the constraint:
+
+  H(Q,C,Q0) = { h|h(x) = w'z in H_Q; w_q = C for q>=Q0 }
+
+  In words: hypothesis set considers Legendre polynomials where any order greater
+  or equal to Q0 gets the weight w_q constrained by some constant C.  
+  Thus, weight of any polynomial of order less than Q0 will be allowed to vary,
+  while the weights greater or equal to Q0 will be fixed to C.
+
+  [c] H(10,0,3) inter H(10,0,4) = H2
+
+  Since 
+    H(10,0,3) = L0 + w1*L1(x) + w2*L2(x) + 0*L3(x)  + 0*L4(x) + ... + 0
+    H(10,0,4) = L0 + w1*L1(x) + w2*L2(x) + w3*L3(x) + 0*L4(x) + ... + 0
+    H(10,0,3) inter H(10,0,4) = L0 +  w1*L1(x) + w2*L2(x) = H2
+
+  The intersection of the 2 hypothesis would be just H2.
+"""
+
+"""
+Neural Networks
+===============
+Watch Lecture 10. This topic is not covered in the book.
+
+Neural network is all about notations. Print your neural network and then code it...
+
+             | 1 <= l <= L             layers
+  w_ij^(l) = | 0 <= i <= d^(l-1)       inputs
+             | 1 <= j <= d^(l)         ouputs
+
+  x_j^(l) = theta( s_j^(l) )  =  theta( sum_{i=0}^{d^(l-1)} w_ij^(l) x_i^(l-1) )
+
+  where theta(s) = tanh(s) = (e^s - e^-s) / (e^s + e^-s)
+
+Apply x to x_1^(0) ... x_{d^(0)}^(0) ->->  x_1^(L) = h(x)
+
+SGD on neural networks:
+
+All the weights w = {w_ij^(l)} determine h(x)
+
+Error on example is (xvec_n, y_n) is
+  e(h(xvec_n), y_n) = e(w)
+
+To implement SGD, we need the gradient
+  
+  Delta e(w) :  partial_d e(w) / partial_d w_ij^(l)  for all i,j,l
+
+    d e(w)/d w_ij^(l) = d e(w) / d s_j^(l)  x  d s_j^(l) / d w_ij^(l)
+
+  we have         d s_j^(l) / d w_ij^(l) = x_i^(l-1)
+  we only need:   d e(w) / d s_j^(l) = delta_j^(l)
+
+  delta for the final layer: l = L and j = 1
+
+    delta_j^(l) = d e(w) / d s_j^(l)  
+    delta_1^(L) = d e(w) / d s_1^(L)  
+    e(w) = (x_1^(L) - y_n)^2
+    x_1^(L) = theta(s_1^(L))
+
+    theta'(s) = 1 - theta(s)^2
+
+
+
+8. A fully connected Neural Network has
+    L = 2
+    d^(0) = 5
+    d^(1) = 3
+    d^(2) = 1
+  If only products of the form
+    w_ij^(l) x_i^(l-1)
+    w_ij^(l) d_j^(l)
+    x_i^(l-1) d_j^(l)
+  count as operations (even for x_0^(l-1) = 1), without counting anything else, which
+  of the following is closest to the total number of operations required in a single
+  iteration of backpropagation (using SGD on one data point?)
+
+  [d] 45
+
+  You have to carefully count the number of operations on 3 areas:
+    
+    1. forward calc for x_ij             :  w_ij^(l) x_i^(l-1)
+    2. backward calc for all the deltas  :  w_ij^(l) d_j^(l)
+    3. updating the weights w_ij         :  x_i^(l-1) d_j^(l)
+  
+    so it's    5*3 + 3*1  + (5*3+3*1) + (5+3+1)  =  18+18+9 = 45
+
+A Neural Network has 10 input units (the constant x_0^(0) is counted here as a unit),
+one output unit, and 36 hidden units (each x_0^(l) is also counted as a unit). The
+hidden units can be arranged in any number of layer l=1,...,L-1,  and each layer is
+fully connected to the layer above it.
+
+* For these problems, refer to the diagram on slide 13
+
+9. What is the minimum possible number of weights that such a network can have?
+
+  If you had just 2 nodes per layer, you end up with 18 layers.
+  The activation for intercept on layer l doesn't exist, so with 2 nodes,
+  you only have 2 w's going into 1 node at next layer.
+  So you will get 
+
+    10*1 + 2*18 = 46
+
+10. What is the maximum possible number of weights that such a network can have?
+
+  max seems to be when you have 2 layers.
+  for example, with 23 nodes on 1st layer, 13 nodes on the 2nd layer
+
+    >>> 10*22 + 23*12 + 13
+    509
+    >>> zip( range(1,36), map(lambda(x): 10*(x-1) + (x)*(36-x-1) + (36-x), range(1,36)))
+    [(1, 69), (2, 110), (3, 149), (4, 186), (5, 221), (6, 254), (7, 285), (8, 314), (9, 341), (10, 366), (11, 389), (12, 410), (13, 429), (14, 446), (15, 461), (16, 474), (17, 485), (18, 494), (19, 501), (20, 506), (21, 509), (22, 510), (23, 509), (24, 506), (25, 501), (26, 494), (27, 485), (28, 474), (29, 461), (30, 446), (31, 429), (32, 410), (33, 389), (34, 366), (35, 341)]
+"""
