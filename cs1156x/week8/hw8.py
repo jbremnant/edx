8af9b9d2a0f93a41426ae5537e66fa855d7b4b0c
jbkim <jbkim@jbkim-lnx.(none)>
2013-11-24 16:50:12 -0500

    commit for wk8

---
+++ cs1156x/week8/hw8.py
@@ -0,0 +1,30 @@
+import sys
+import math
+import re
+import random as rn
+import numpy as np
+from sklearn import svm
+import scipy.optimize as sci
+
+"""
+import sys
+sys.path.append('/home/jbkim/Development/edx/cs1156x/week8')
+sys.path.append('/home/jbkim/Development/edx/cs1156x/week7')
+import hw8 as h
+reload(h)
+"""
+
+"""
+Primal versus Dual Problem
+==========================
+
+
+"""
+
+
+"""
+Support Vector Machines With Soft Margins
+=========================================
+
+
+"""
